65843a8af2c1c694d7416edd0d3f61df5320b4c5
thejoaov <jvictorsantos852@gmail.com>
2019-08-07 23:26:36 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,18 +1,19 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const port = process.env.PORT || 3333
 
-const routes = require("./routes");
+const routes = require('./routes')
 
-const server = express();
+const server = express()
 
 mongoose.connect(
-  "mongodb+srv://thejoaov:naruto@cluster0-mgfky.gcp.mongodb.net/omnistack8?retryWrites=true&w=majority",
+  'mongodb+srv://thejoaov:naruto@cluster0-mgfky.gcp.mongodb.net/omnistack8?retryWrites=true&w=majority',
   { useNewUrlParser: true }
-);
+)
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+server.use(cors())
+server.use(express.json())
+server.use(routes)
 
-server.listen(3333);
+server.listen(port)
