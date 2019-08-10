03d5edab92c814f2b7e67a7565590470f0db38b8
thejoaov <jvictorsantos852@gmail.com>
2019-08-10 10:22:09 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const url = process.env.REACT_APP_DATABASE_URL;
+const url =
+  process.env.REACT_APP_DATABASE_URL ||
+  "https://sem-omni-backend.herokuapp.com";
 
 const api = axios.create({
   baseURL: url,
